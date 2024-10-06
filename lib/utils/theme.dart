e4f444517a2507286a820803d31840728e120f39
Paulo Henrique Moschen Sartori <moschenhenrique@hotmail.com>
2024-10-06 18:41:30 -0300

    FIX - ajuste no thema do app

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -4,23 +4,23 @@ class ClientTheme {
   static ThemeData mainTheme = ThemeData(
     useMaterial3: true,
 
-    primarySwatch: Colors.blue,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.blue,
+    ),
+    
     scaffoldBackgroundColor: const Color.fromARGB(223, 245, 245, 245),
 
-    // Personalização do appBar
     appBarTheme: const AppBarTheme(
       centerTitle: true,
       backgroundColor: Color(0xFF64B5F6),
       titleTextStyle: TextStyle(color: Colors.black, fontSize: 20),
     ),
 
-    // Personalização do listTile
     listTileTheme: const ListTileThemeData(
       textColor: Color(0xFF212121),
       iconColor: Color(0xFF757575),
     ),
 
-    // Personalização do floatingActionButton
     floatingActionButtonTheme: const FloatingActionButtonThemeData(
       backgroundColor: Color(0xFF64B5F6),
       foregroundColor: Colors.black,
@@ -28,27 +28,26 @@ class ClientTheme {
 
     dividerColor: const Color(0xFFBDBDBD),
 
-    // Personalização do TimePicker
     timePickerTheme: TimePickerThemeData(
       backgroundColor: Colors.white,
       dialBackgroundColor: const Color(0xFF64B5F6),
       dialHandColor: Colors.white,
-      hourMinuteShape: const RoundedRectangleBorder(
-        side: BorderSide(
-          color: Color(0xFF2196F3),
-          width: 2.0,
-        ),
-      ),
       hourMinuteColor: Colors.white,
       hourMinuteTextColor: Colors.black,
-
+      inputDecorationTheme: const InputDecorationTheme(
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        border: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+      ),
       cancelButtonStyle: ButtonStyle(
         textStyle: WidgetStateProperty.all(
           const TextStyle(color: Colors.black, fontSize: 16.0),
         ),
         foregroundColor: WidgetStateProperty.all(Colors.black),
       ),
-
       confirmButtonStyle: ButtonStyle(
         textStyle: WidgetStateProperty.all(
           const TextStyle(color: Colors.blue, fontSize: 16.0),
@@ -57,7 +56,6 @@ class ClientTheme {
       ),
     ),
 
-    // Personalização do text
     textTheme: const TextTheme(
       bodyMedium: TextStyle(
         fontWeight: FontWeight.w500,
